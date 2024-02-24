17050e60aaaeb92b144c3cf9d0e3969867fd96de
Meowlang <87633743+Meowlanguage@users.noreply.github.com>
2024-02-24 21:00:04 +0330

    Add files via upload

---
+++ Meow.cpp
@@ -0,0 +1,42 @@
+ #include "Meow.h"
+
+int main(int argc, char const *argv[])
+{
+	Meow lang;
+	
+
+	for(int i=1;i< argc;i++){
+		//cout<<"|"<<argv[i]<<"|"<<endl;
+		string ag;
+		ag = argv[i];
+		if(lang.exists_(ag)){
+			vector<string> s = lang.readfile(argv[i]);
+			for(int j =0;j< s.size();j++){
+				//lang.line = i;
+				//cout<<j<<endl;
+				lang.worker(s[j],true);
+			}
+		}
+		if(ag == "-e"){
+			lang.worker(argv[i+1],false);
+		}else if(ag == "-h"){
+ 			lang.help(0);
+ 		}else if(ag == "-v"){
+			cout<<"Meow "<<lang.data["VERSION"]<<endl;
+		}else if((ag == "-e")||(argv[i+1])){
+			lang.decode(argv[i+1]);
+		}}
+		if(!argv[1]){
+			string test;
+			while(true){
+			cout<<">>> ";
+			getline(cin,test);
+			if(!lang.terminalcmd(test))
+				lang.worker(test,false);
+			cout<<endl;
+			}
+		}
+		
+	
+	return 0;
+}
